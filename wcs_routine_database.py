d2fcb4884d8d2d5d6dd295951fe43077234824da
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2025-08-20 20:37:55 +0200

    a

--- wcs_routine_database.py
+++ wcs_routine_database.py
@@ -98,8 +98,8 @@ video_txt_search = st.text_input("Routine title search:").lower().split(',')
 routine_vids = (df
                 .pipe(just_a_peek)
                 .with_columns(all_ = pl.concat_str(pl.all().cast(pl.String), separator=' '))
-                .filter(pl.col('all_').str.contains_any(video_txt_search, ascii_case_insensitive=True),
-                      )
+                # .filter(pl.col('all_').str.contains_any(video_txt_search, ascii_case_insensitive=True),
+                #       )
                 .with_columns(search_terms = pl.col('Title')
                                                 .str.to_lowercase()
                                                 .str.extract_all('|'.join(video_txt_search))
